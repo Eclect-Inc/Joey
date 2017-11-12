a4cc9c94a7655ae145b823788aaf9d913e7796e2
Jumar Martin <jumar@eclect.xyz>
2017-11-11 21:00:19 -0500

    Cleaned Welcome.kt, simplified the "Don't have an account" button.

--- app/src/main/java/com/eclect/codenamejoey/Welcome.kt
+++ app/src/main/java/com/eclect/codenamejoey/Welcome.kt
@@ -1,14 +1,9 @@
 package com.eclect.codenamejoey
 
-import android.graphics.Typeface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Html
-import android.text.Spannable
-import android.text.SpannableString
 import kotlinx.android.synthetic.main.activity_welcome.*
 import android.text.SpannableStringBuilder
-import android.text.style.StyleSpan
 
 
 class Welcome : AppCompatActivity() {
@@ -18,17 +13,10 @@ class Welcome : AppCompatActivity() {
         setTheme(R.style.AppThemeSimple)
         setContentView(R.layout.activity_welcome)
 
-//        val prompt = SpannableStringBuilder()
-//        prompt.append("Don't have an account?")
-//        val start = prompt.length
-//        prompt.append(" Sign up!")
-//        val boldSpan = StyleSpan(android.graphics.Typeface.BOLD)
-//        prompt.setSpan(StyleSpan(Typeface.BOLD),start, prompt.length, Spannable.SPAN_EXCLUSIVE_INCLUSIVE)
-//
-//
-//
-//
-//        btnReg.setText(prompt)
+        val prompt = SpannableStringBuilder()
+        prompt.append("Don't have an account?")
+
+        btnReg.setText(prompt.toString())
     }
 
 
