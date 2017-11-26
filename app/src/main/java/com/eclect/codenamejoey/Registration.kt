dce92fe9c8202d961fac04ed3957785577ea0ff7
Jumar Martin <jumar@eclect.xyz>
2017-11-25 20:37:55 -0500

    *** Added Firebase Authentication Integration *** *** Updated gradle to 3.0.1 (3.0.0 -> 3.0.1) *** *** Updated Kotlin to 1.1.61 (1.1.51 -> 1.1.61) *** *** Downgraded JDK langLevel to JDK_1_7 *** *** Raised compile+target SDK version to 27 *** *** Raised min SDK Version to 21 ***
    + Added Registration.kt + activity_registration.xml, for reg activity
    
    - Welcome.kt
      + Firebase Authentication
      + Buttons now do things, btnRegistration now points to reg activity
      - imports we haven't used
    
    - Layouts & Values
      + from hardcoded to strings.xml for localization in the future

--- app/src/main/java/com/eclect/codenamejoey/Registration.kt
+++ app/src/main/java/com/eclect/codenamejoey/Registration.kt
@@ -1,5 +1,23 @@
 package com.eclect.codenamejoey
 
-/**
- * Created by jumarmartin on 11/13/17.
- */
+import android.content.Context
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Registration : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+
+
+    companion object {
+        fun newIntent(context: Context): Intent {
+            val intent = Intent(context,  Registration::class.java)
+            return intent
+        }
+    }
+
+}
--- app/src/main/java/com/eclect/codenamejoey/Welcome.kt
+++ app/src/main/java/com/eclect/codenamejoey/Welcome.kt
@@ -2,18 +2,81 @@ package com.eclect.codenamejoey
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_welcome.*
+import android.support.design.widget.Snackbar
+import android.view.View
+import android.widget.EditText
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import mehdi.sakout.fancybuttons.FancyButton
 
 
 class Welcome : AppCompatActivity() {
+
+    private val fbAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.AppThemeSimple)
         setContentView(R.layout.activity_welcome)
-        
-        
+
+        val btnLogin = findViewById<FancyButton>(R.id.btn_login)
+        val fieldEmail  = findViewById<EditText>(R.id.editText)
+        val fieldPassword = findViewById<EditText>(R.id.editText2)
+        val btnRegistration = findViewById<FancyButton>(R.id.btnReg)
+
+        btnRegistration.setOnClickListener {
+
+            val intent = Registration.newIntent(this)
+            startActivity(intent)
+
+        }
+
+        btnLogin.setOnClickListener { view ->
+
+            val email = fieldEmail.text.toString()
+            val password = fieldPassword.text.toString()
+
+
+            if (email.isEmpty() || password.isEmpty()){
+
+                showMessage(view, "Please enter both your email and password!")
+
+            } else {
+
+                signIn(view, email, password)
+
+            }
+
+        }
+
     }
 
+
+    private fun signIn(view: View, email: String, password: String){
+
+        fbAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this,
+                OnCompleteListener<AuthResult> { task ->
+            if (task.isSuccessful){
+
+                //TODO: Usher in a NEW activity, the main activity. HOORAY!
+
+                showMessage(view, "Logged In")
+
+            }else {
+
+                showMessage(view, "either your email or password is incorrect!")
+            }
+
+        } )
+    }
+
+
+    private fun showMessage(view: View, message: String){
+
+        Snackbar.make(view, message, Snackbar.LENGTH_SHORT).setAction("Action", null)
+                .show()
+
+    }
 
 }
